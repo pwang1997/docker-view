a07edd359ca0cd0dfa4642415f3f55d8e607311c
Zhengliang Wang <zwang684@uottawa.ca>
2023-02-19 22:47:14 -0500

    added shell command dialog

--- src/components/CommandDialog.tsx
+++ src/components/CommandDialog.tsx
@@ -3,14 +3,16 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-
-
-const contextTitle = "";
+import { IconButton, Snackbar, TextField } from '@mui/material';
+import axios from 'axios';
+import CloseIcon from '@mui/icons-material/Close';
 
 function CommandDialog() {
     const [open, setOpen] = React.useState(false);
+    const [openSnackbar, setOpenSnackbar] = React.useState(false);
+    const [shellCommand, setShellCommand] = React.useState("");
+    const errorMessageRef = React.useRef("");
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -20,10 +22,67 @@ function CommandDialog() {
         setOpen(false);
     };
 
+
+    const handleSnackbarClose = () => {
+        setOpenSnackbar(false);
+    }
+
+    const handleSubmit = () => {
+        axios.post(
+            "http://localhost/api/v1/container/invoke",
+            {
+                command: shellCommand
+            })
+            .then((res) => {
+                console.log(res.data);
+                setShellCommand("");
+                handleClose();
+            })
+            .catch((err) => {
+                errorMessageRef.current = err?.response?.data
+                setShellCommand("");
+                setOpenSnackbar(true);
+            });
+    }
+
+    const renderSnackbar = React.useCallback(() => {
+        return (
+            <Snackbar
+                open={openSnackbar}
+                autoHideDuration={2000}
+                onClose={handleSnackbarClose}
+                message={errorMessageRef.current}
+                action={action}
+            />
+        )
+    }, [openSnackbar])
+
+
+    const handleSnackbarButtonClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        handleSnackbarClose();
+      };
+
+    const action = (
+        <React.Fragment>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleSnackbarButtonClose}
+            >
+                <CloseIcon fontSize="small" />
+            </IconButton>
+        </React.Fragment>
+    );
+
     return (
         <div>
             <Button variant="outlined" onClick={handleClickOpen}>
-                Open alert dialog
+                Shell Command
             </Button>
             <Dialog
                 open={open}
@@ -31,19 +90,24 @@ function CommandDialog() {
                 aria-labelledby="command-dialog-label"
             >
                 <DialogTitle id="command-dialog-label">
-                    {"Use Google's location service?"}
+                    Use Shell Command:
                 </DialogTitle>
                 <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Let Google help apps determine location. This means sending anonymous
-                        location data to Google, even when no apps are running.
-                    </DialogContentText>
+                    <TextField
+                        id="shell-input"
+                        size="small"
+                        label="Shell Command"
+                        variant="outlined"
+                        value={shellCommand}
+                        onChange={(e) => setShellCommand(e.target.value)}
+                    />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Submit</Button>
+                    <Button onClick={handleSubmit}>Submit</Button>
                 </DialogActions>
             </Dialog>
+            {renderSnackbar()}
         </div>
     )
 }
